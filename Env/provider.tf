62f89fe0d29ee988cb386f11603361938f23c45a
demiking91 <kashyapashish29@gmail.com>
2024-02-11 18:12:47 +0530

    backend config

--- Env/provider.tf
+++ Env/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.85.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "Jyoti_rg_centralIndia"
+    storage_account_name = "terraformtest12346"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
